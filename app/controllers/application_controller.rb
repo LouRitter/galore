2ba79d07eee80d7816018500a7b270cddb7bcba2
louritter <louritter1@gmail.com>
2022-02-11 00:06:19 -0500

    set up service object to make api call and root route to display json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    def events
+        @galore = GaloreEventService.new()
+        @events = @galore.get_events
+        render json: JSON.pretty_generate(JSON.parse(@events.to_s))
+    end
 end
--- app/services/galore_event_service.rb
+++ app/services/galore_event_service.rb
@@ -1,8 +1,12 @@
+require 'faraday/net_http'
+
 class GaloreEventService 
-    def initialize()
-    end
+
 
     def get_events
-
+        conn = Faraday.new()
+        res = conn.get("https://api.getgalore-staging.com/v1/events", {}, {"Api-Key": ENV['GALORE_API_KEY']})
+  
+        return res.body
     end
 end
