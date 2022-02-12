8f0157361e612dbac6e6b83f88ec575b179c1e7c
louritter <louritter1@gmail.com>
2022-02-12 13:57:20 -0500

    added default count and page value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
 
     def events
-        @page = params[:page] 
-        @count = params[:count]
+        @page = params[:page] ||= 1
+        @count = params[:count] ||= 15
         @events = JSON.parse(GaloreEventService.call({count: @count, page: @page}))
         @total = @events["total"]
         @last_page = @events["last_page"]
